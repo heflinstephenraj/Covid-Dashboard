5f4c226e683c8ce490ba793290e1fa3d57203adc
heflinstephenraj <heflinstephen03@gmail.com>
2021-04-24 17:33:33 +0530

    Show total active cases on the selcted date of selcted country

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from htbuilder.funcs import rgba, rgb
 
 st.set_page_config(page_title="Covid Dashboard", page_icon="🕸", layout='wide', initial_sidebar_state='expanded')
 
+head , data_info = st.beta_columns(2)
 
+head.title("Covid Dashboard")
+data_info.write('Data is obtained from [JHU CSSE COVID-19 Data](https://github.com/CSSEGISandData/COVID-19)')
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -29,23 +32,37 @@ def fetch_data(url):
   data.columns=columns
   return data
 
-confirmed = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-death = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
-recovered = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
-
-col1, col2, col3 = st.beta_columns(3)
-
 def last_update(data):
   monthDict = {"1":'January', "2":'February',"3": 'March',"4": 'April', "5":'May',"6":'June', "7":'July', "8":'August', "9":'September', "10":'October', "11":'November',"12":'December'}
   last_update = list(data.columns)[-1].split("/")
   last_update = last_update[1]+" "+monthDict[last_update[0]]+" 20"+last_update[-1]
   return last_update
 
-col1.write("Confirmed cases last updated on "+last_update(confirmed))
-col2.write("Death cases last updated on "+last_update(death))
-col3.write("Recovered cases last updated on "+last_update(recovered))
+def convert_date(date,option):
+    if option == 1:
+        monthDict = {"1":'January', "2":'February',"3": 'March',"4": 'April', "5":'May',"6":'June', "7":'July', "8":'August', "9":'September', "10":'October', "11":'November',"12":'December'}
+        date=date.split("/")
+        date = date[1]+" "+monthDict[date[0]]+" 20"+date[-1]
+        return date
+    elif option == 2:
+        monthDict ={'January':'1','February':'2','March':'3','April':'4','May':'5','June':'6','July':'7','August':'8','September':'9','October':'10','November':'11','December':'12'}
+        date=date.split(" ")
+        print(date)
+        date=monthDict[date[1]]+"/"+date[0]+"/"+date[-1][-2:]
+        return date
 
-if st.checkbox('Show raw data'):
+confirmed = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+death = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+recovered = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+
+confirmed_update, death_update, recovered_update = st.beta_columns(3)
+confirmed_update.write("Confirmed cases last updated on "+last_update(confirmed))
+death_update.write("Death cases last updated on "+last_update(death))
+recovered_update.write("Recovered cases last updated on "+last_update(recovered))
+
+country_option , empty, date_option  = st.beta_columns(3)
+
+if data_info.checkbox('Show raw data'):
     st.subheader('Raw data')
     option = st.selectbox('Please Select the type of data.',('Confirmed cases', 'Deaths', 'Recovered',"All"))
     if option == 'Confirmed cases':
@@ -61,6 +78,22 @@ if st.checkbox('Show raw data'):
       st.write(death)
       st.write("Recovered")
       st.write(recovered)
+else:
+  dates = []
+  countries = []
+  for i in list(confirmed['country/region']):
+    if i not in countries:
+      countries.append(i)
+  country = country_option.selectbox('Please Select the Country',countries,help="Deisplay the data of selcted country")
+  for i in list(confirmed[confirmed['country/region']==country].columns)[4:]:
+    dates.append(convert_date(i,1))
+  date = date_option.select_slider("Select the date", options=dates, value=None,  key=None, help="Select the date to compare deaths, new cases and recovered cases")
+  date = convert_date(date,2)
+  st.write(f"Total active cases on {date} of {country}: {int(confirmed[confirmed['country/region']==country][date])}")
+  
+  
+  
+
 
 
 
