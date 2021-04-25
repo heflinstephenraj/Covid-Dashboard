40afa42bacd399d9e5c60619896578fe7f68f739
heflinstephenraj <heflinstephen03@gmail.com>
2021-04-25 21:43:19 +0530

    Added white theme

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit
 
-echo "[server]
+echo '[theme]
+primaryColor="#f63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#f0f2f6"
+textColor="#262730"
+font="sans serif"
+[server]
 headless = true
 port = $PORT
 enableCORS = false
-" > ~/.streamlit/config.toml
+' > ~/.streamlit/config.toml
